febc35624c76d72bf945edcc35a8399f790c9acf
Adrian Cortes <ls-adrian@hotmail.com>
2021-07-23 16:48:00 -0500

    add refactoring

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//Modulos
 const express = require("express");
 const passport = require("passport");
-const session = require("express-session");
+const session = require('./utils/session.conf');
 const path = require("path");
-const authRouter = require('./routes/auth.routes');
 
+//Rutas
+const authRouter = require('./routes/auth.routes');
+const catRouter = require('./routes/category.routes');
 
 require('./config/passport');
 
@@ -21,34 +24,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Middleware de terceros
-app.use(session({
-    secret: "academlo secret",
-    resave: false,
-    saveUninitialized: true
-}));
+app.use(session);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Middleware de aplicación
-//req -> Request (solicitud/petición del usuario)
-//res -> Response (respuesta hacía el usuario)
-//next -> Es continuar con la ejecución del siguiente middleware
-//next(error) -> Al momento de recibir un valor como argumento, se ejecutará el middleware para el manejo de errores
+//Middleware de aplicació
 app.get("/", (req, res, next) => {
     res.render("pages/home", {title: "Inicio"});
 });
 
 app.use(authRouter);
-
-//Restringir el acceso a los usuarios que no están autenticados
-//Implementar un sistema de inicio de sesión y registro
-app.get("/categorias", (req, res, next) => {
-    if(req.isAuthenticated()){
-        let username = `${req.user.firstname} ${req.user.lastname}`;
-        return res.render("pages/categories", {title: "Categorias", username});
-    }
-    return res.redirect("/login");
-});
+app.use(catRouter);
 
 module.exports = app;
